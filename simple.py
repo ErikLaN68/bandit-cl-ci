acd9f9f9b1fb0e8f5f12f0e9ba91c19a8d9000bf
ErikLaN68 <eriklaneave@gmail.com>
2024-09-17 15:56:51 -0400

    should fail

--- simple.py
+++ simple.py
@@ -1,8 +1,11 @@
 ## Test files used
 import os
+import hashlib
 
 os.system("touch test.txt")
 
 file = open("test.txt", "a")
 file.write("this is a test file")
 file.close()
+
+should_fail = hashlib.md5(b'What will happend')
